f0d777685756d7cf7434b373502e80614b9cb66e
Sakshiprasad1927 <123757922+Sakshiprasad1927@users.noreply.github.com>
2024-07-06 12:32:20 +0530

    Add files via upload

---
+++ restaurant/p2.js
@@ -0,0 +1,13 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let foods = document.getElementById('food');
+let food3s = document.getElementById('food3');
+let food4s = document.getElementById('food4');
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('https://i.pinimg.com/originals/e1/da/d5/e1dad5315972c8a9db86fb01d69c7ecb.jpg')";
+})
+
+food2s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('https://www.archanaskitchen.com/images/archanaskitchen/0-Affiliate-Articles/RESTAURANT_STYLE_SOUTH_INDIAN_THALI_original.jpg')";
+})
